9f134434ca2af5a2be30dc5228322e78ae18aefa
Paul <pje224@nyu.edu>
2021-03-18 19:35:32 -0400

    Create Product model & schema

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 
 const express = require('express');
 const connectDB = require('./config/db');
+const products = require('./routes/api/products');
 
 const app = express();
 connectDB();
 
 app.get('/', (req, res) => res.send('Hello world!'));
+app.use('/api/products', products);
 
 const port = process.env.PORT || 8082;
 app.listen(port, () => console.log(`Server running on port ${port}`));
---
+++ models/Product.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  price: {
+    type: Number,
+  },
+  quantity: {
+    type: Number,
+  },
+  updated_date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Product = mongoose.model("product", ProductSchema);
