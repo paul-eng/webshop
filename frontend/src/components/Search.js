4e6ba0b1700648408e54d8724e21df7a6f04ba05
Paul <pje224@nyu.edu>
2021-04-25 18:52:52 -0400

    Implement text index on Item schema

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchSearch } from "../actions/ItemActions";
+import { getFilters } from "../actions/FilterActions";
 import ContentArea from "./ContentArea";
 import { queryStr } from "../util/Util";
 
 class Search extends Component {
   componentDidMount() {
-      let query = queryStr(this.props)
-    this.props.fetchSearch(query)
+    let query = queryStr(this.props);
+    this.props.fetchSearch(query).then(this.props.getFilters);
   }
+  componentDidUpdate() {
+    let query = queryStr(this.props);
+    this.props.fetchSearch(query);
+  }
+
   render() {
     return (
       <div className="Search">
@@ -21,6 +27,7 @@ class Search extends Component {
 const mapDispatchToProps = (dispatch) => {
   return {
     fetchSearch: (query) => dispatch(fetchSearch(query)),
+    getFilters: () => dispatch(getFilters()),
   };
 };
 
