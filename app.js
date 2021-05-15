d0988b83ef76512b9e8c1a79896fd02dba9dabd4
Paul <pje224@nyu.edu>
2021-05-15 11:10:19 -0400

    Change statis express

--- app.js
+++ app.js
@@ -21,18 +21,18 @@ app.use("/api/checkout", checkout);
 
 // app.get("/", (req, res) => res.send("Hello world!"));
 
-// app.use(express.static(path.join(__dirname, '../build')))
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../build'))
-// })
+app.use(express.static(path.join(__dirname, '../build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build'))
+})
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("frontend/build"));
-    const path = require('path');
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-    });
-  }
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static("frontend/build"));
+//     const path = require('path');
+//     app.get('*', (req, res) => {
+//       res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+//     });
+//   }
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Server running on port ${port}`));
