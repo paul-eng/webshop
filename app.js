085abd94732163609d90aecd5bf8b2cf43874f99
Paul <pje224@nyu.edu>
2021-05-15 10:39:16 -0400

    Try diff static method

--- app.js
+++ app.js
@@ -20,10 +20,13 @@ app.use("/api/carts", carts);
 app.use("/api/orders", orders);
 app.use("/api/checkout", checkout);
 
-app.use(express.static(path.join(__dirname, '../build')))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build'))
-})
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Server running on port ${port}`));
