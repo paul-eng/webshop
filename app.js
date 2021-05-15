cad2ccc445bd68e747ce71f6278b8189546a8fda
Paul <pje224@nyu.edu>
2021-05-15 11:50:04 -0400

    Modify path for get

--- app.js
+++ app.js
@@ -20,20 +20,19 @@ app.use("/api/carts", carts);
 app.use("/api/orders", orders);
 app.use("/api/checkout", checkout);
 
-// app.get("/", (req, res) => res.send("Hello world!"));
+// app.use(express.static(path.join(__dirname, 'frontend/build')))
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'frontend/build'))
+// })
 
-app.use(express.static(path.join(__dirname, 'frontend/build')))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend/build'))
-})
 
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static("frontend/build"));
-//     const path = require('path');
-//     app.get('*', (req, res) => {
-//       res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-//     });
-//   }
+app.use(express.static(path.join(__dirname, "frontend", "build")))
+
+// ...
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+});
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Server running on port ${port}`));
